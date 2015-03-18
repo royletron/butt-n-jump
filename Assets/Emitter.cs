705bcc9c2b6ee64325de6850c99ac0c5d413905d
Darren Royle <darren.royle@oup.com>
2015-03-18 22:10:48 +0000

    Updated emitters and added camera tracking and player movement

--- Assets/Emitter.cs
+++ Assets/Emitter.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class Emitter : MonoBehaviour {
 
-	public GameObject prefab;
+	public Rigidbody2D prefab;
+	public float emitRate = 2.56f;
+	public int position = 2;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (transform.position.x >= (emitRate*position)) {
+			Rigidbody2D instance = (Rigidbody2D) Instantiate(prefab, transform.position, transform.rotation);
+			position = position + 1;
+		}
 	}
 }
