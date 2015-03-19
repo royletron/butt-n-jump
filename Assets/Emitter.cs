4bb4719c5cbe888aed8cf8c1a63578b537689e64
Darren Royle <darren.royle@oup.com>
2015-03-19 21:56:42 +0000

    Updated emitter to work with velocities

--- Assets/Emitter.cs
+++ Assets/Emitter.cs
@@ -5,14 +5,15 @@ public class Emitter : MonoBehaviour {
 
 	public Rigidbody2D[] prefabs;
 	public bool continuous = true;
-	public float emitRate = 2.56f;
-	public int position = 2;
+	private float childWidth = 2.56f;
 	public bool random = false;
 	public float minDelay = 1.0f;
 	public float maxDelay = 2.0f;
 
 	private float timeCounter = 0.0f;
 	private float timeTarget;
+
+	private Rigidbody2D instance;
 
 
 	private float OffsetX;
@@ -23,10 +24,22 @@ public class Emitter : MonoBehaviour {
 		if (random) {
 			SetTime();
 		}
+		if (continuous) {
+			childWidth = prefabs [Random.Range (0, prefabs.Length)].GetComponent<SpriteRenderer>().bounds.size.x;
+			AddContinuous();
+		}
 	}
 
 	void SetTime(){
 		timeTarget = Random.Range(minDelay*100, maxDelay*100)/100;
+	}
+
+	void AddContinuous(){
+		float childX = transform.position.x;
+		if (instance != null) {
+			childX = instance.transform.position.x + childWidth;
+		}
+		instance = (Rigidbody2D)Instantiate (prefabs[Random.Range(0,prefabs.Length)], new Vector2 (childX, transform.position.y), transform.rotation);
 	}
 	
 	// Update is called once per frame
@@ -34,10 +47,8 @@ public class Emitter : MonoBehaviour {
 		transform.position = new Vector2(Camera.main.transform.position.x + OffsetX, transform.position.y);
 		//print (transform.position);
 		if (continuous) {
-			if (transform.position.x >= (emitRate * position)) {
-				Rigidbody2D instance = (Rigidbody2D)Instantiate (prefabs[Random.Range(0,prefabs.Length)], new Vector2 (emitRate * position, transform.position.y), transform.rotation);
-				//instance.position = new Vector2(emitRate*position, instance.position.y);
-				position = position + 1;
+			if (transform.position.x >= instance.position.x) {
+				AddContinuous();
 			}
 		} else {
 			if (random) {
